c5f3342918abb7f640da3c003cbd0d6b95f2cbcd
Yulisa M <31376002+YulisaM2@users.noreply.github.com>
2022-04-20 12:20:37 -0500

    Revert "Seed db: Games + Users"

--- src/controllers/util/util.js
+++
@@ -1,7 +0,0 @@
-const handle = (promise) => {
-    return promise
-        .then(data => ([data, undefined]))
-        .catch(error => Promise.resolve([undefined, error]));
-}
-
-module.exports.handle = handle;
