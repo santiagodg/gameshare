11c77dd4e944ae9d29a048f11d45edfeb2bee4a1
ym2 <yulisa.med2@gmail.com>
2022-04-30 09:46:36 -0500

    Send all the lists for viewing at home
    If the user is logged in and accesses home, an object with all the lists created sorted from most recent to less recent is available for frontend.

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,13 +1,16 @@
 var express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
+const List = require('../models/list')
 
 var router = express.Router()
 
-router.get('/', function (req, res) {
+router.get('/', async function (req, res) {
     if (!req.isAuthenticated()) {
         res.render('home/landing-page', { page: 'landing' })
     } else {
-        res.render('home/home')
+        const lists = await List.find().sort({_id: -1})
+        res.render('home/home', { lists :lists })
     }
 })
 
