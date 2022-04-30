4f2c613aa77b9076d9fa02db86fd78a0752a2aef
ym2 <yulisa.med2@gmail.com>
2022-04-30 11:28:55 -0500

    Create list without games
    Post request now works with all the model's values except games.

--- src/controllers/list.js
+++ src/controllers/list.js
@@ -2,16 +2,37 @@ const express = require('express')
 const req = require('express/lib/request')
 const mongoose = require('mongoose')
 const { isLoggedIn } = require('../middleware')
+const { handle } = require('./util/util')
 const Game = require('../models/game')
 const List = require('../models/list')
 
 var router = express.Router()
+
+router.get('/', isLoggedIn, async (req, res) =>{
+    res.render('list/collection')
+})
 
 router.get('/new', isLoggedIn, async (req, res) =>{
     const games = await Game.find().sort({ name: 1 })
     // console.log(games)
 
     res.render('list/new', { games : games })
+})
+
+router.post('/', isLoggedIn, async (req, res) =>{
+    const list = new List(req.body)
+    list.author = req.user_id
+
+    const [newList, error] = await handle(list.save())
+
+    if(error){
+        console.log(error)
+        return res.status(400).render('bad-request')
+    }
+
+    // req.flash('List added successfully.')
+    res.redirect('/list')
+    // res,redirect('/')
 })
 
 module.exports = router
