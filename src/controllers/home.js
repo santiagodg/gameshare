ec97f46cdc1e96ecc0839c361a246f61d74c463d
ym2 <yulisa.med2@gmail.com>
2022-04-30 20:21:06 -0500

    Minor fixup: Populating array attributes of lists and games

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,7 @@
 var express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const { handle } = require('./util/util')
 const List = require('../models/list')
 
 var router = express.Router()
@@ -9,7 +10,10 @@ router.get('/', async function (req, res) {
     if (!req.isAuthenticated()) {
         res.render('home/landing-page', { page: 'landing' })
     } else {
-        const lists = await List.find().sort({_id: -1})
+        const [lists, listsError] = await handle(List.find().sort({  _id : -1 }).populate(['likes', 'games', 'comments']).exec())
+        if(listsError){
+            return res.status(500).render('server-error')
+        }
         res.render('home/home', { lists :lists })
     }
 })
