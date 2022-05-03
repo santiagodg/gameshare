b805cf141ceddde5dcbe0b1bbadd93e3732a0421
Santiago Díaz Guevara <santidiazg@hotmail.es>
2022-05-03 18:08:04 -0500

    Add user administration

--- src/controllers/admin/admin.js
+++ src/controllers/admin/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 const GameModel = require('../../models/game');
+const UserModel = require('../../models/user');
 
 const { handle } = require('../util/util');
 const { isAdmin } = require('../../middleware');
@@ -108,6 +109,103 @@ router.delete('/games/:gameID', isAdmin, async (req, res) => {
 
     if (opResult.modifiedCount < 1) {
         return res.status(404).redirect('/admin/games');
+    }
+
+    res.sendStatus(200);
+});
+
+router.get("/users", isAdmin, async (_, res) => {
+    const [users, usersError] = await handle(UserModel.find().exec());
+
+    if (usersError) {
+        console.log(usersError);
+
+        return res.status(500).send(usersError);
+    }
+
+    res.render('admin/users', { users });
+});
+
+router.post('/users', isAdmin, async function (req, res) {
+    if (!('username' in req.body) || !('email' in req.body)) {
+        return res.status(400).send("'username' and 'email' properties must be set on body of request")
+    }
+
+    // Check if it already exists
+    const [existentUser, existentError] = await handle(UserModel.findOne({ username: req.body.username }).exec());
+    if (existentError) {
+        console.log(existentError);
+        return res.status(500).send(existentError);
+    }
+    if (existentUser !== null) {
+        // req.flash('error', 'A user with that username already exists.')
+        return res.redirect('back')
+    }
+
+    const user = new UserModel(req.body);
+    const [_, userSaveError] = await handle(user.save());
+
+    if (userSaveError) {
+        console.log(userSaveError);
+        return res.status(500).send(userSaveError);
+    }
+
+    const [existent1User, existent1Error] = await handle(UserModel.findOne({ username: req.body.username }).exec())
+    if (existent1Error) {
+        console.log(existent1Error);
+        return res.status(500).send(existent1Error);
+    }
+
+    res.send(existent1User)
+})
+
+router.put('/users/:userID', isAdmin, async function (req, res) {
+    let [user, userError] = await handle(UserModel.findOne({ _id: req.params.userID }).exec());
+
+    if (userError || user === undefined) {
+        console.log(userError);
+
+        return res.status(404).send(userError);
+    }
+
+    user.username = req.body.username;
+
+    // Check if it already exists
+    const [existentUser, existentError] = await handle(UserModel.findOne({ username: user.username }).exec());
+    if (existentError) {
+        console.log(existentError);
+        return res.status(500).send(existentError);
+    }
+    if (existentUser !== null && !existentUser._id.equals(req.params.userID)) {
+        // req.flash('error', 'A game with that name already exists.')
+        return res.redirect('back')
+    }
+
+    user.isAdmin = req.body.isAdmin;
+    user.deleted = req.body.deleted;
+
+    [user, userError] = await handle(user.save());
+
+    if (userError) {
+        console.log(userError);
+
+        return res.status(400).send(userError);
+    }
+
+    const [existent1User, existent1Error] = await handle(UserModel.findOne({ username: req.body.username }).exec())
+    if (existent1Error) {
+        console.log(existent1Error);
+        return res.status(500).send(existent1Error);
+    }
+
+    res.send(existent1User)
+})
+
+router.delete('/users/:userID', isAdmin, async (req, res) => {
+    const opResult = await UserModel.deleteOne({ _id: req.params.userID }).exec();
+
+    if (opResult.modifiedCount < 1) {
+        return res.status(404).redirect('/admin/users');
     }
 
     res.sendStatus(200);
