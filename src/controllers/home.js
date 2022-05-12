12529993768139070dbc555f1abee6ddf00cfef9
ym2 <yulisa.med2@gmail.com>
2022-05-12 01:19:07 -0500

    Complete fuzzy search for list name
    On home we can search and clear based on text inputted

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,7 @@
 var express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
-const { handle } = require('./util/util')
+const { handle, escapeRegex } = require('./util/util')
 const List = require('../models/list')
 
 var router = express.Router()
@@ -10,11 +10,29 @@ router.get('/', async function (req, res) {
     if (!req.isAuthenticated()) {
         res.render('home/landing-page', { page: 'landing' })
     } else {
-        const [lists, listsError] = await handle(List.find({ deleted: false }).sort({ _id: -1 }).populate(['author', 'likes', 'games', 'comments']).exec())
-        if (listsError) {
-            return res.status(500).render('server-error', { user: req.user })
+        if (req.query.search) {
+            const regex = new RegExp(escapeRegex(req.query.search), 'gi')
+            const [lists, listsError] = await handle(List.find({ name: regex, deleted: false }).sort({ 'name' : 'asc' }).populate(['author', 'likes', 'games', 'comments']).exec())
+    
+            if (listsError) {
+                 res.status(404).render('not-found')
+                return
+            }
+            if (lists.length < 1) {
+                req.flash('error', 'No list name matched, please try again!')
+                res.redirect('back')
+            } else {
+                res.render('home/home', { lists: lists, searched_name: req.query.search, user: req.user })
+            }
+    
+        } else {
+
+            const [lists, listsError] = await handle(List.find({ deleted: false }).sort({ _id: -1 }).populate(['author', 'likes', 'games', 'comments']).exec())
+            if (listsError) {
+                return res.status(500).render('server-error', { user: req.user })
+            }
+            res.render('home/home', { lists: lists, searched_name: undefined, user: req.user })
         }
-        res.render('home/home', { lists: lists, user: req.user })
     }
 })
 
